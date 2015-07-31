04eee9c0833c39a11dd2f3db136d1ccb245d4bf9
LindaRN <this.is.original.eh@gmail.com>
2015-07-31 16:07:56 -0400

    version 2
    created main and content divs

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php
 
   echo "this is index page"
+ ?> 
+  	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
 
-?>
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
+
