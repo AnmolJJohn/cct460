b130609b15403e33ea24bbfb1eb0120c09e3094e
LindaRN <this.is.original.eh@gmail.com>
2015-08-05 09:53:56 -0400

    got the logo working
    logo now appears as a link in the header

--- header.php
+++ header.php
@@ -24,18 +24,20 @@
 
 	<header id="masthead" class="site-header" role="banner">
 		<div class="site-branding">
+
 			<?php if ( is_front_page() && is_home() ) : ?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?><img src="http://i996.photobucket.com/albums/af87/ThisIsUnoriginal/cupcakelogo.png" alt=" Artstery Cupcakes logo links to the home page" title="Back to Home"></a></h1>
 			<?php else : ?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?><img src="http://i996.photobucket.com/albums/af87/ThisIsUnoriginal/cupcakelogo.png" alt=" Artstery Cupcakes logo links to the home page" title="Back to Home"></a></p>
 			<?php endif; ?>
 			<p class="site-description"><?php bloginfo( 'description' ); ?></p>
+
 		</div><!-- .site-branding -->
-	<nav id="site-navigation" class="main-navigation" role="navigation">
+			<nav id="site-navigation" class="main-navigation" role="navigation">
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'cupcake' ); ?></button>
+			
 			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 		</nav><!-- #site-navigation -->
-
 	</header><!-- #masthead -->
 
 
