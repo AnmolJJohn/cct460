2134290407fd68356ad076ec10de15bd42a0fa14
arifnabi <nabilarif1@gmail.com>
2015-08-04 21:28:20 -0400

    Update functions.php

--- functions.php
+++ functions.php
@@ -109,7 +109,7 @@ function cupcake_widgets_init() {
 add_action( 'widgets_init', 'cupcake_widgets_init' );
 
 /**
- * Enqueue scripts and styles.
+ * Enqueue both scripts and styles.
  */
 function cupcake_scripts() {
 	wp_enqueue_style( 'cupcake-style', get_stylesheet_uri() );
@@ -118,6 +118,9 @@ function cupcake_scripts() {
 
 	wp_enqueue_script( 'cupcake-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
 
+/* If a singular post is displayed and it is open for comments and a comment thread is available
+allow users to respond to specific individual comments. */
+	
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
